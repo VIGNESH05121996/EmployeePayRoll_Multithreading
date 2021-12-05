07b024b34d5c5cfe95ef16d3380582c74b6f73b8
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-12-05 18:55:15 +0530

    [VIGNESH M] Added: Welcome Message

---
+++ EmployeePayroll_Multithreading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayroll_Multithreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Payroll Multithreading");
+        }
+    }
+}
